f65317eab4be8e44759dc393390bb8c356ec5d65
ilechka <ilja.slugin@yandex.ru>
2024-01-20 21:52:53 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if (isset($_SESSION['username'])) {
+    echo "Рад приветствовать тебя на сайте, {$_SESSION['username']}!";
+    echo '<br>';
+    echo '<a href="/exit.php">Выйти</a>';
+} else {
+    include('form.html');
+}
+echo '<br>';
+include('author.html');
