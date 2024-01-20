686dfb8ea9ed0e5998a1bb8d2fa7982373dd73f8
ilechka <ilja.slugin@yandex.ru>
2024-01-20 22:35:48 +0300

    second commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION['username'])) {
     echo "Рад приветствовать тебя на сайте, {$_SESSION['username']}!";
     echo '<br>';
-    echo '<a href="/exit.php">Выйти</a>';
+    echo '<a href="./exit.php">Выйти</a>';
 } else {
     include('form.html');
 }
